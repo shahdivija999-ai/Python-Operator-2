08170fa54a486a519f9cec83b7f3f8242d2c6942
shahdivija999-ai <shahdivija999@gmail.com>
2026-05-27 16:46:19 -0500

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,24 @@
+a = 10 
+b = 12
+c = 0 
+
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:
+    print("Atleast one number has boolean value as False")
+      
+      
+      
+a = 10 
+b = 12
+c = 0 
+
+if a > 0 and b > 0 and c > 0:
+    print("All the numbers are greater than 0")
+else: 
+    print("Atleast one number is not greater than 0")
+
+if b > 0 or c > 0:
+    print("Atleast one number is greater than 0")
+else:
+    print("None of the numbers are greater than 0")
